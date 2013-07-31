5b62b83f10edd5e9cde612e39d7ed431c0dbecfc
Sarah Vessels <cheshire137@gmail.com>
2013-07-30 22:42:08 -0400

    Adding more screenshots, Google Analytics

--- extension/popup.js
+++ extension/popup.js
@@ -15,6 +15,19 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-42849115-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script');
+  ga.type = 'text/javascript';
+  ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(ga, s);
+})();
+
 var turntable_spotter_popup = {
   get_spotify_track_search_url: function(query) {
     return 'http://ws.spotify.com/search/1/track.json?q=' +
